e9ab742b553d592177c5067a44ac1ffa3af96d2d
Bartek Stodulkiewicz <tiersen89@gmail.com>
2019-07-28 15:10:14 +0200

    initial commit

---
+++ src/main/java/pl/bartlomiejstodulkiewicz/serwisszkolen/SerwisSzkolenApplication.java
@@ -0,0 +1,13 @@
+package pl.bartlomiejstodulkiewicz.serwisszkolen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisSzkolenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerwisSzkolenApplication.class, args);
+	}
+
+}
